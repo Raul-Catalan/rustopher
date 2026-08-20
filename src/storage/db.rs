67c9d8fe72005dde2342aa94d53b818f70b30b1e
Raul Catalan <raulrcatalan@gmail.com>
2026-08-20 08:26:24 -0700

    Add Basic Task Schema

--- src/storage/db.rs
+++ src/storage/db.rs
@@ -6,7 +6,7 @@ pub fn init_db(db_path: &str) -> Result<Connection> {
     conn.execute_batch("PRAGMA foreign_keys = ON")?;
 
     // Create tables if they don't exist
-    // crate::task::create_table(&conn)?;
+    crate::task::create_table(&conn)?;
     // crate::time::create_table(&conn)?;
 
     Ok(conn)
